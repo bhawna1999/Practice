fca01536413de649ea11730b3616ecad571b3087
bhawna1999 <32545043+bhawna1999@users.noreply.github.com>
2020-10-01 20:02:04 +0530

    Update facti.cpp

--- facti.cpp
+++ facti.cpp
@@ -6,12 +6,12 @@ int main()
 int n;
 cout<<"Enter a number : ";
 cin>>n;
-int fact = 1;
-for(int i=1;i<=n;i++)
+
+for( i=1;i<=n;i++)
 {
 fact = fact * i;
 }
 cout<<"Factorial is : "<<fact;
-return 0;
+
 
 }
